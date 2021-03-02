343f62cdfac7233a724d100ed0b2738f9b52bd45
Carlos Hurtado <gabo_duty@hotmail.com>
2021-03-01 20:15:13 -0500

    Datatable

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 $(document).ready(function () {
+  $(document).ready(function () {
+    $("#table_list").DataTable();
+  });
+  getListado();
+});
+
+function tableData(data) {
+  $(".dataTable").dataTable().fnClearTable();
+
+  for (let i = 0; i < data.length; i++) {
+    var rowIndex = $(".dataTable")
+      .dataTable()
+      .fnAddData([
+        data[i].radi_nume_radi,
+        data[i].usua_nomb,
+        data[i].depe_nomb,
+        data[i].anex_codigo,
+      ]);
+  }
+}
+
+function getListado() {
   $.get("list_radicado.php", function (res, err) {
     let listado = JSON.parse(res);
-    console.log(listado);
     let td = "";
-    for (let row of listado) {
-      td = `<tr>
-            <td>${row.radi_nume_radi}</td>
-            <td>${row.usua_nomb}</td>
-            <td>${row.depe_nomb}</td>
-            <td>${row.anex_codigo}</td> </tr>`;
 
-      $("#contenido").append(td);
-    }
+    tableData(listado);
+    //for (let row of listado) {
+    //  td = `<tr>
+    //        <td>${row.radi_nume_radi}</td>
+    //        <td>${row.usua_nomb}</td>
+    //        <td>${row.depe_nomb}</td>
+    //        <td>${row.anex_codigo}</td> </tr>`;
+    //
+    //  $("#contenido").append(td);
+    //}
   });
-});
+}
--- index.php
+++ index.php
@@ -5,24 +5,43 @@
     <meta charset="UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+
     <script src="index.js"></script>
 
     <title>Document</title>
 </head>
 
 <body>
-    <table id="table" class="table table-striped">
-        <tr>
-            <th>Numero Radicado</th>
-            <th>Anexos</th>
-            <th>Dependencia</th>
-            <th>Usuario</th>
-        </tr>
-        <tbody id="contenido">
+    <div class="row">
+        <div class="col-md-12">
+            &nbsp;
+        </div>
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <h2>Listado de dependencias</h2>
+                    <table id="table_list" class="table table-striped datetable">
+                        <thead>
+                            <tr>
+                                <th class="text_center">Numero Radicado</th>
+                                <th class="text_center">Anexos</th>
+                                <th class="text_center">Dependencia</th>
+                                <th class="text_center">Usuario</th>
+                            </tr>
+                        </thead>
+                        <tbody id="contenido">
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
 
-        </tbody>
-    </table>
 
 </body>
 
